829423fa332df3f8c2cb0ebdcb5110379cb4bc58
antonio <antonio@pykiss.com>
2017-09-16 15:18:43 +0200

    añadir repositorio

--- package.js
+++ package.js
@@ -4,6 +4,7 @@ Package.describe({
   name: 'hacknlove:reactive-interval',
   version: '0.0.1',
   summary: 'recalculate each interval',
+  git: 'https://github.com/hacknlove/reactive-interval',
   documentation: 'README.md'
 })
 
